9aff9eb553764cc5cfd3b5a6591b34a17b5f9a5a
Chau Tran <cctran96@gmail.com>
2021-03-02 02:48:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,32 +1,3 @@
-function basicTeenager(age) {
-  if (age > 12 && age < 20) {
-    return "You are a teenager!"
-  }
-}
-
-function teenager(age) {
-  if (age > 12 && age < 20) {
-    return "You are a teenager!"
-  }
-  return "You are not a teenager"
-}
-
-function ageChecker(age) {
-  if (age > 12 && age < 20) {
-    return "You are a teenager!"
-  } else if (age < 13) {
-    return "You are a kid"
-  } else {
-    return "You are a grownup"
-  }
-}
-
-function ternaryTeenager(age) {
-  return (age > 12 && age < 20) ? "You are a teenager":"You are not a teenager"
-
-function switchAge(age) {
-  if (age > 12 && age < 20) {
-    return "You are a teenager"
-  }
-  return "You have an age"
+function ternaryTeenager() {
+  return (age > 12 && age < 20) ? "You are a teenager":"You are not a teengar"
 }
